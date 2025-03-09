e5f65fd222ccc52f6dbaf54738a8087bfbeb5f22
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-03-09 16:01:41 -0700

    Hmm, need to restart chat.

--- tt/podman-swift-monofile.sh
+++ tt/podman-swift-monofile.sh
@@ -7,6 +7,7 @@ PODMAN_CONFIG="pdvm-rbw"  # Your podman configuration
 
 echo "Running Swift code in a temporary container..."
 podman -c "$PODMAN_CONFIG" run --rm \
+    --network host \
     -v "./Study:/app" \
     "$CONTAINER_IMAGE" \
     swift "/app/study-swift-monofile/monofile.swift"
