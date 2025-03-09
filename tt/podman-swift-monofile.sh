f167704d562777a085cfe386bddd9c915ffc6c4a
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-03-09 15:52:28 -0700

    Launcher.

---
+++ tt/podman-swift-monofile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Configuration variables
+PROJECT_DIR="./Study/study-swift-monofile"
+CONTAINER_IMAGE="ghcr.io/bhyslop/recipemuster:test_sftrr.20250309__222204"
+PODMAN_CONFIG="pdvm-rbw"  # Your podman configuration
+
+echo "Running Swift code in a temporary container..."
+podman -c "$PODMAN_CONFIG" run --rm \
+    -v "$(pwd)/Study:/app" \
+    "$CONTAINER_IMAGE" \
+    swift "/app/study-swift-monofile/monofile.swift"
+
+echo "Execution complete."
+
+
